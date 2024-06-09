f148dc60cb1a5831d29b856b0f3dbd5a88965465
Teo <thod_ntonas@hotmail.com>
2024-06-09 22:05:05 +0300

    Create abstract repository for crud methods

---
+++ Repositories/BaseRepository.cs
@@ -0,0 +1,57 @@
+﻿
+using HealthcareAppointmentApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthcareAppointmentApp.Repositories
+{
+    public abstract class BaseRepository<T> : IBaseRepository<T> where T : class
+    {
+        protected readonly HealthCareDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        protected BaseRepository(HealthCareDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public virtual async Task<T?> AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            return entity;
+        }
+
+        public virtual async Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+            return entities;
+        }
+
+        public virtual async Task<T?> DeleteAsync(int id)
+        {
+            T? existing = await _dbSet.FindAsync(id);
+            if (existing is null)
+            {
+                return null;
+            }
+            _dbSet.Remove(existing);
+            return existing;
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public virtual async Task<T?> GetAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _dbSet.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
---
+++ Repositories/IBaseRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthcareAppointmentApp.Repositories
+{
+    public interface IBaseRepository<T>
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T?> GetAsync(int id);
+        Task<T?> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+        void Update(T entity);
+        Task<T?> DeleteAsync(int id);
+    }
+}
