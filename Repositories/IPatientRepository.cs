ec44c57c53b0a8becf1abc86b419767f93a167fe
Teo <thod_ntonas@hotmail.com>
2024-06-10 01:46:17 +0300

    Create patient repository

---
+++ Repositories/IPatientRepository.cs
@@ -0,0 +1,9 @@
+﻿using HealthcareAppointmentApp.Data;
+
+namespace HealthcareAppointmentApp.Repositories
+{
+    public interface IPatientRepository
+    {
+        Task<IEnumerable<Patient>> GetPatientsByLastname(string lastname);
+    }
+}
---
+++ Repositories/PatientRepository.cs
@@ -0,0 +1,15 @@
+﻿using HealthcareAppointmentApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthcareAppointmentApp.Repositories
+{
+    public class PatientRepository : BaseRepository<Patient>, IPatientRepository
+    {
+        public PatientRepository(HealthCareDbContext context) : base(context) { }
+
+        public async Task<IEnumerable<Patient>> GetPatientsByLastname(string lastname)
+        {
+            return await _context.Patients.Where(p => p.Lastname.Contains(lastname)).ToListAsync();
+        }
+    }
+}
