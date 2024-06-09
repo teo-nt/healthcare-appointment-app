9ad72d6972ec5782d631a1633490f39bcd464be7
Teo <thod_ntonas@hotmail.com>
2024-06-09 22:12:51 +0300

    Create User repository

---
+++ Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using HealthcareAppointmentApp.Data;
+
+namespace HealthcareAppointmentApp.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetUserAsync(string username, string password);
+        Task<User?> GetByUsernameAsync(string username);
+    }
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using HealthcareAppointmentApp.Data;
+using HealthcareAppointmentApp.Security;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthcareAppointmentApp.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(HealthCareDbContext context) : base(context) { }
+
+        /// <summary>
+        /// Returns the user based on username.
+        /// </summary>
+        /// <param name="username">The username of user.</param>
+        /// <returns>The user if the username exists, null otherwise.</returns>
+        public async Task<User?> GetByUsernameAsync(string username)
+        {
+            return await _context.Users.Where(u => u.Username == username).FirstOrDefaultAsync();
+        }
+
+        /// <summary>
+        /// Gets the user based on username and password.
+        /// </summary>
+        /// <param name="username">The username of user.</param>
+        /// <param name="password">The password of user.</param>
+        /// <returns>The user if username and password are valid, null otherwise.</returns>
+        public async Task<User?> GetUserAsync(string username, string password)
+        {
+            User? user = await _context.Users.Where(u => u.Username == username).FirstOrDefaultAsync();
+            if (user is null) return null;
+            if (!EncryptionUtil.IsValidPassword(password, user.Password)) return null;
+            return user;
+        }
+    }
+}
