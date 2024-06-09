f0d5a5e0830956ea99008b9aad5c6e98b4a8c79d
Teo <thod_ntonas@hotmail.com>
2024-06-10 02:47:39 +0300

    Create Doctor repository

---
+++ Repositories/DoctorRepository.cs
@@ -0,0 +1,37 @@
+﻿using HealthcareAppointmentApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthcareAppointmentApp.Repositories
+{
+    public class DoctorRepository : BaseRepository<Doctor>, IDoctorRepository
+    {
+        public DoctorRepository(HealthCareDbContext context) : base(context)
+        {
+        }
+
+        public async Task<IEnumerable<Doctor>> GetDoctorsByCity(string city)
+        {
+            return await _context.Doctors.Where(d => d.City == city).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Doctor>> GetDoctorsByCityAndSpeciality(string city, string speciality)
+        {
+            return await _context.Doctors
+                .Where(d => d.City == city && d.Speciality != null && d.Speciality.SpecialityName == speciality)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Doctor>> GetDoctorsByLastname(string lastname)
+        {
+            return await _context.Doctors.Where(d => d.Lastname.Contains(lastname)).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Doctor>> GetDoctorsBySpeciality(string speciality)
+        {
+            return await _context.Doctors
+                .Where(d => d.Speciality != null 
+                        && d.Speciality.SpecialityName.Contains(speciality))
+                .ToListAsync();
+        }
+    }
+}
---
+++ Repositories/IDoctorRepository.cs
@@ -0,0 +1,12 @@
+﻿using HealthcareAppointmentApp.Data;
+
+namespace HealthcareAppointmentApp.Repositories
+{
+    public interface IDoctorRepository
+    {
+        Task<IEnumerable<Doctor>> GetDoctorsByLastname(string lastname);
+        Task<IEnumerable<Doctor>> GetDoctorsBySpeciality(string speciality);
+        Task<IEnumerable<Doctor>> GetDoctorsByCity(string city);
+        Task<IEnumerable<Doctor>> GetDoctorsByCityAndSpeciality(string city, string speciality);
+    }
+}
