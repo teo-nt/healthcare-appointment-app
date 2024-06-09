377e30ac2a10e97dc0f46003e4f3e1fbcd5fa67c
Teo <thod_ntonas@hotmail.com>
2024-06-09 22:04:13 +0300

    Provide utility class for encrypting passwords in the db

---
+++ Security/EncryptionUtil.cs
@@ -0,0 +1,17 @@
+﻿using BCrypt.Net;
+
+namespace HealthcareAppointmentApp.Security
+{
+    public static class EncryptionUtil
+    {
+        public static string EncryptPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool IsValidPassword(string plainPassword, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(plainPassword, hashedPassword);
+        }
+    }
+}
