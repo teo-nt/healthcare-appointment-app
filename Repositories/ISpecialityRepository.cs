f00e84d94905b65e912d8e60a092ad6fb27773c6
Teo <thod_ntonas@hotmail.com>
2024-06-12 04:30:16 +0300

    Create speciality repository

---
+++ Repositories/ISpecialityRepository.cs
@@ -0,0 +1,9 @@
+﻿using HealthcareAppointmentApp.Data;
+
+namespace HealthcareAppointmentApp.Repositories
+{
+    public interface ISpecialityRepository
+    {
+        Task<Speciality?> GetSpecialityByNameAsync(string specialityName);
+    }
+}
---
+++ Repositories/SpecialityRepository.cs
@@ -0,0 +1,17 @@
+﻿using HealthcareAppointmentApp.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HealthcareAppointmentApp.Repositories
+{
+    public class SpecialityRepository : BaseRepository<Speciality>, ISpecialityRepository
+    {
+        public SpecialityRepository(HealthCareDbContext context) : base(context)
+        {
+        }
+
+        public async Task<Speciality?> GetSpecialityByNameAsync(string specialityName)
+        {
+            return await _context.Specialities.Where(s => s.SpecialityName == specialityName).FirstOrDefaultAsync();
+        }
+    }
+}
