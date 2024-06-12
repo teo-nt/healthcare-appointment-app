e238bb68209097e7823e1dd2c50fca3f62fe4a05
Teo <thod_ntonas@hotmail.com>
2024-06-13 01:18:01 +0300

    Create mapper class and global exception handler

---
+++ Configuration/MapperConfig.cs
@@ -0,0 +1,37 @@
+﻿using AutoMapper;
+using HealthcareAppointmentApp.Data;
+using HealthcareAppointmentApp.DTO;
+
+namespace HealthcareAppointmentApp.Configuration
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig() 
+        {
+            CreateMap<User, DoctorReadOnlyDTO>()
+                .ForMember(dest => dest.User, opt => opt.MapFrom(src => src))
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Doctor!.Id))
+                .ForMember(dest => dest.Firstname, opt => opt.MapFrom(src => src.Doctor!.Firstname))
+                .ForMember(dest => dest.Lastname, opt => opt.MapFrom(src => src.Doctor!.Lastname))
+                .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.Doctor!.City))
+                .ForMember(dest => dest.Address, opt => opt.MapFrom(src => src.Doctor!.Address))
+                .ForMember(dest => dest.StreetNumber, opt => opt.MapFrom(src => src.Doctor!.StreetNumber))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.Doctor!.PhoneNumber))
+                .ForMember(dest => dest.AppointmentDuration, opt => opt.MapFrom(src => src.Doctor!.AppointmentDuration))
+                .ForMember(dest => dest.Speciality, opt => opt.MapFrom(src => src.Doctor!.Speciality));
+
+            CreateMap<User, PatientReadOnlyDTO>()
+                .ForMember(dest => dest.User, opt => opt.MapFrom(src => src))
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Patient!.Id))
+                .ForMember(dest => dest.Firstname, opt => opt.MapFrom(src => src.Patient!.Firstname))
+                .ForMember(dest => dest.Lastname, opt => opt.MapFrom(src => src.Patient!.Lastname))
+                .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.Patient!.Age))
+                .ForMember(dest => dest.Gender, opt => opt.MapFrom(src => src.Patient!.Gender))
+                .ForMember(dest => dest.MedicalHistory, opt => opt.MapFrom(src => src.Patient!.MedicalHistory))
+                .ForMember(dest => dest.PhoneNumber, opt => opt.MapFrom(src => src.Patient!.PhoneNumber));
+
+            CreateMap<User, UserReadOnlyDTO>();
+            CreateMap<Speciality, SpecialityReadOnlyDTO>();
+        }
+    }
+}
---
+++ Helpers/CustomExceptionHandler.cs
@@ -0,0 +1,28 @@
+﻿using HealthcareAppointmentApp.Service.Exceptions;
+using Microsoft.AspNetCore.Diagnostics;
+using System.Net;
+
+namespace HealthcareAppointmentApp.Helpers
+{
+    public class CustomExceptionHandler : IExceptionHandler
+    {
+        public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, 
+                Exception exception, CancellationToken cancellationToken)
+        {
+            var response = httpContext.Response;
+
+            response.StatusCode = exception switch
+            {
+                DoctorAlreadyExistsException or
+                InvalidCredentialsException or
+                PatientAlreadyExistsException or
+                UserAlreadyExistsException => (int)HttpStatusCode.BadRequest,
+
+                UserNotFoundException => (int)HttpStatusCode.NotFound,
+                _ => (int)HttpStatusCode.InternalServerError
+            };
+            await response.WriteAsJsonAsync(new { message = exception.Message }, cancellationToken);
+            return true;
+        }
+    }
+}
