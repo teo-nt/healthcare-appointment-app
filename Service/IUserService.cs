6d7ad1ce0fbfa95b92093b3a8a1de8a1edaeaba8
Teo <thod_ntonas@hotmail.com>
2024-06-12 04:40:27 +0300

    Implement services for User

---
+++ Service/IUserService.cs
@@ -0,0 +1,71 @@
+﻿using HealthcareAppointmentApp.Data;
+using HealthcareAppointmentApp.DTO;
+using HealthcareAppointmentApp.Models;
+
+namespace HealthcareAppointmentApp.Service
+{
+    public interface IUserService
+    {
+        /// <summary>
+        /// Signs up a user as a patient.
+        /// </summary>
+        /// <param name="dto">The patient containing all user details.</param>
+        /// <returns>The created user</returns>
+        /// <exception cref="Exceptions.PatientAlreadyExistsException"
+        /// <exception cref="Exceptions.UserAlreadyExistsException"
+        Task<User> SignUpUserPatientAsync(PatientSignUpDTO dto);
+
+        /// <summary>
+        /// Signs up a user as a doctor.
+        /// </summary>
+        /// <param name="dto">The doctor containing all user details.</param>
+        /// <returns>The created user</returns>
+        /// <exception cref="Exceptions.UserAlreadyExistsException"
+        /// <exception cref="Exceptions.DoctorAlreadyExistsException"
+        Task<User?> SignUpUserDoctorAsync(DoctorSignUpDTO dto);
+
+        /// <summary>
+        /// Returns the user based on loginDTO (username and password).
+        /// </summary>
+        /// <param name="loginDTO">The dto containing username and password.</param>
+        /// <returns>The user with the credentials provided if valid.</returns>
+        /// <exception cref="Exceptions.InvalidCredentialsException"
+        Task<User> LoginUserAsync(LoginDTO loginDTO);
+
+        /// <summary>
+        /// Updates the user's email and password.
+        /// </summary>
+        /// <param name="dto">Entity that contains email and password.</param>
+        /// <returns>The updated user.</returns>
+        /// <exception cref="Exceptions.UserAlreadyExistsException"
+        /// <exception cref="Exceptions.UserNotFoundException"
+        Task<User> UpdateUserAsync(UserUpdatePasswordEmailDTO dto);
+
+        /// <summary>
+        /// Returns the user that has the provided username.
+        /// </summary>
+        /// <param name="username">Username of user to be found.</param>
+        /// <returns>The user with the requested username.</returns>
+        /// <exception cref="Exceptions.UserNotFoundException"
+        Task<User> GetUserByUsernameAsync(string username);
+
+        /// <summary>
+        /// Creates a JWT with provided claims.
+        /// </summary>
+        /// <param name="userId">Id of user.</param>
+        /// <param name="username">Username of user.</param>
+        /// <param name="email">Email of user.</param>
+        /// <param name="role">Role of user.</param>
+        /// <param name="securityKey">Key used for signature.</param>
+        /// <returns>The JWT.</returns>
+        string CreateUserToken(int userId, string username, string email, UserRole role, String securityKey);
+
+        /// <summary>
+        /// Deletes a user based on id. Exception is thrown if user is not found.
+        /// </summary>
+        /// <param name="id">Id of user.</param>
+        /// <returns>The deleted user.</returns>
+        /// <exception cref="Exceptions.UserNotFoundException"
+        Task<User> DeleteUserAsync(int id);
+    }
+}
---
+++ Service/UserService.cs
@@ -0,0 +1,219 @@
+﻿using HealthcareAppointmentApp.Data;
+using HealthcareAppointmentApp.DTO;
+using HealthcareAppointmentApp.Models;
+using HealthcareAppointmentApp.Repositories;
+using HealthcareAppointmentApp.Security;
+using HealthcareAppointmentApp.Service.Exceptions;
+
+namespace HealthcareAppointmentApp.Service
+{
+    public class UserService : IUserService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ILogger<UserService> _logger;
+
+        public UserService(IUnitOfWork unitOfWork, ILogger<UserService> logger)
+        {
+            _unitOfWork = unitOfWork;
+            _logger = logger;
+        }
+
+        public string CreateUserToken(int userId, string username, string email, UserRole role, string securityKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User> DeleteUserAsync(int id)
+        {
+            try
+            {
+                User? user = await _unitOfWork.UserRepository.DeleteAsync(id);
+                if (user is null) throw new UserNotFoundException($"User with id {id} does not exist");
+                await _unitOfWork.SaveAsync();
+                _logger.LogInformation($"User with id: {id} was deleted");
+                return user;
+            }
+            catch (UserNotFoundException e)
+            {
+                _logger.LogWarning($"Error in delete - {e.Message}");
+                throw;
+            }
+        }
+
+        public async Task<User> GetUserByUsernameAsync(string username)
+        {
+            try
+            {
+                User? user = await _unitOfWork.UserRepository.GetByUsernameAsync(username);
+                if (user is null) throw new UserNotFoundException($"User with username {username} does not exist");
+                _logger.LogInformation($"User with username: {username} was retrieved");
+                return user;
+            }
+            catch (UserNotFoundException e)
+            {
+                _logger.LogWarning($"Error in retrieving user - {e.Message}");
+                throw;
+            }
+        }
+
+        public async Task<User> LoginUserAsync(LoginDTO loginDTO)
+        {
+            try
+            {
+                User? user = await _unitOfWork.UserRepository.GetUserAsync(loginDTO.Username, loginDTO.Password);
+                if (user is null) throw new InvalidCredentialsException
+                        ($"Failure in login with username: {loginDTO.Username}");
+                _logger.LogInformation($"User with username: {loginDTO.Username} logged in");
+                return user;
+            }
+            catch (InvalidCredentialsException e)
+            {
+                _logger.LogWarning($"Error in user login - {e.Message}");
+                throw;
+            }
+        }
+
+        public async Task<User?> SignUpUserDoctorAsync(DoctorSignUpDTO dto)
+        {
+            User? user;
+            Doctor? doctor;
+            Speciality? speciality;
+            string specialityName = dto.Speciality;
+
+            try
+            {
+                user = ExtractUser(dto);
+                User? existingUser = await _unitOfWork.UserRepository.GetByUsernameOrEmailAsync(user.Username, user.Email);
+                if (existingUser is not null) throw new UserAlreadyExistsException
+                        ($"User with username: {user.Username} or email: {user.Email} already exists");
+                user.Password = EncryptionUtil.EncryptPassword(user.Password);
+                user.UserRole = UserRole.Doctor;
+                user.Status = UserStatus.Pending;
+                
+                doctor = ExtractDoctor(dto);
+                Doctor? existingDoctor = await _unitOfWork.DoctorRepository.GetDoctorByPhoneNumber(doctor.PhoneNumber);
+                if (existingDoctor is not null) throw new DoctorAlreadyExistsException
+                        ($"Doctor with phone number: {doctor.PhoneNumber} already exists");
+
+                Speciality? existingSpeciality = await _unitOfWork.SpecialityRepository.GetSpecialityByNameAsync(specialityName);
+                if (existingSpeciality is null) 
+                {
+                    speciality = await _unitOfWork.SpecialityRepository.AddAsync(new Speciality { SpecialityName = specialityName });
+                }
+                else
+                {
+                    speciality = existingSpeciality;
+                }
+                await _unitOfWork.UserRepository.AddAsync(user);
+                await _unitOfWork.DoctorRepository.AddAsync(doctor);
+                doctor.User = user;
+                doctor.Speciality = speciality;
+                await _unitOfWork.SaveAsync();
+                _logger.LogInformation($"Doctor with username: {user.Username} signed up successfully.");
+                return user;
+            }
+            catch (Exception e) when (e is UserAlreadyExistsException || e is DoctorAlreadyExistsException)
+            {
+                _logger.LogWarning($"Error signing up doctor -- {e.Message}");
+                throw;
+            }
+        }
+
+        public async Task<User> SignUpUserPatientAsync(PatientSignUpDTO dto)
+        {
+            User? user;
+            Patient? patient;
+
+            try
+            {
+                user = ExtractUser(dto);
+                User? existingUser = await _unitOfWork.UserRepository.GetByUsernameOrEmailAsync(user.Username, user.Email);
+                if (existingUser is not null) throw new UserAlreadyExistsException
+                        ($"User with username: {user.Username} or email: {user.Email} already exists");
+                user.Password = EncryptionUtil.EncryptPassword(user.Password);
+                user.UserRole = UserRole.Patient;
+                user.Status = UserStatus.Approved;
+
+                patient = ExtractPatient(dto);
+                Patient? existingPatient = await _unitOfWork.PatientRepository.GetPatientByPhoneNumber(patient.PhoneNumber);
+                if (existingPatient is not null) throw new PatientAlreadyExistsException
+                        ($"Patient with phone number: {patient.PhoneNumber} already exists");
+                await _unitOfWork.UserRepository.AddAsync(user);
+                await _unitOfWork.PatientRepository.AddAsync(patient);
+                patient.User = user;
+                await _unitOfWork.SaveAsync();
+                _logger.LogInformation($"Patient with username: {user.Username} signed up successfully.");
+                return user;
+            }
+            catch (Exception e) when (e is PatientAlreadyExistsException || e is UserAlreadyExistsException)
+            {
+                _logger.LogWarning($"Error signing up patient -- {e.Message}");
+                throw;
+            }
+        }
+
+        public async Task<User> UpdateUserAsync(UserUpdatePasswordEmailDTO dto)
+        {
+            User? user;
+
+            try
+            {
+                user = await _unitOfWork.UserRepository.GetAsync(dto.Id);
+                if (user is null) throw new UserNotFoundException($"User with id: {dto.Id} does not exist");
+                User? existingUserEmail = await _unitOfWork.UserRepository.GetByEmailAsync(dto.Email);
+                if (existingUserEmail is not null && existingUserEmail.Id != dto.Id)
+                {
+                    throw new UserAlreadyExistsException($"User with email: {user.Email} already exists");
+                }
+                user.Password = EncryptionUtil.EncryptPassword(dto.Password);
+                user.Email = dto.Email;
+                await _unitOfWork.SaveAsync();
+                _logger.LogInformation($"User with username: {user.Username} was updated successfully");
+                return user;
+            }
+            catch (Exception e) when (e is UserNotFoundException || e is UserAlreadyExistsException)
+            {
+                _logger.LogWarning($"Error updating user -- {e.Message}");
+                throw;
+            }
+        }
+
+        private User ExtractUser(UserSignUpDTO dto)
+        {
+            return new User
+            {
+                Username = dto.Username,
+                Password = dto.Password,
+                Email = dto.Email
+            };
+        }
+
+        private Patient ExtractPatient(PatientSignUpDTO dto)
+        {
+            return new Patient
+            {
+                Firstname = dto.Firstname,
+                Lastname = dto.Lastname,
+                Age = dto.Age,
+                Gender = dto.Gender,
+                MedicalHistory = dto.MedicalHistory,
+                PhoneNumber = dto.PhoneNumber
+            };
+        }
+
+        private Doctor ExtractDoctor(DoctorSignUpDTO dto)
+        {
+            const uint APPOINTMENT_DEFAULT_DURATION = 30;
+            return new Doctor
+            {
+                Firstname = dto.Firstname,
+                Lastname = dto.Lastname,
+                City = dto.City,
+                Address = dto.Address,
+                StreetNumber = dto.StreetNumber,
+                PhoneNumber = dto.PhoneNumber,
+                AppointmentDuration = dto.AppointmentDuration ?? APPOINTMENT_DEFAULT_DURATION
+            };
+        }
+    }
+}
