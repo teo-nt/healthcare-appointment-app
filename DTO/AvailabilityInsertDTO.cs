814b713ee77905028df0c67becdf1a349a660b62
Teo <thod_ntonas@hotmail.com>
2024-06-18 07:38:35 +0300

    Add dto for appointment availability

---
+++ DTO/AvailabilityInsertDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace HealthcareAppointmentApp.DTO
+{
+    public class AvailabilityInsertDTO
+    {
+        public DateOnly Date { get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
+    }
+}
---
+++ DTO/AvailableTimeslotDTO.cs
@@ -0,0 +1,13 @@
+﻿using HealthcareAppointmentApp.Models;
+
+namespace HealthcareAppointmentApp.DTO
+{
+    public class AvailableTimeslotDTO
+    {
+        public long Id { get; set; }
+        public DateOnly Date { get; set; }
+        public TimeOnly StartTimeSlot { get; set; }
+        public TimeOnly EndTimeSlot { get; set; }
+        public AvailabilityStatus Status { get; set; }
+    }
+}
